bcd382936e50114424929a29a97e8c7ddf6efc97
Leandro Martins <leandro.martins.85@gmail.com>
2021-11-02 22:50:28 -0300

    Food Refatorado

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,15 +2,19 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background: #c72828;
-  padding: 30px 0;
-
+  padding: 30px 0;  
+  
   header {
-    width: 1280px;
+    max-width: 1280px; 
     margin: 0 auto;
     padding: 0 0 160px;
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    @media (max-width: 970px) {
+      padding: 0 10px 160px;
+    }
 
     nav {
       div {
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -4,11 +4,15 @@ export const FoodsContainer = styled.div`
   width: 100%;
   max-width: 1280px;
   margin: 0 auto;
-  padding: 40px 0;
-  margin-top: -140px;
+  padding: 40px 10px;
+  margin-top: -140px; 
 
   display: grid;
 
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 32px;
+
+  @media (max-width: 970px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
